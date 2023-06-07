5b9a06d6eeb40460511994c50c99dadc75d80cd7
Gustl22 <git@reb0.org>
2023-06-07 17:23:21 +0200

    ci: flutter_release action

--- scripts/build-apk.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -x
-
-# Build 
-flutter build apk \
-  --dart-define=APP_ENVIRONMENT=development \
-  --dart-define=API_URL="https://server.wrestling-scoreboard.oberhauser.dev/api" \
-  --dart-define=WEB_SOCKET_URL="wss://server.wrestling-scoreboard.oberhauser.dev/ws"
--- scripts/build-windows.bat
+++
@@ -1,6 +0,0 @@
-powershell flutter build windows^
- --dart-define=APP_ENVIRONMENT=development^
- --dart-define=API_URL="https://server.wrestling-scoreboard.oberhauser.dev/api"^
- --dart-define=WEB_SOCKET_URL="wss://server.wrestling-scoreboard.oberhauser.dev/ws"
-
-powershell Compress-Archive -Force -Path build\windows\runner\Release\* -DestinationPath build\windows\wrestling-scoreboard-client-windows.zip
